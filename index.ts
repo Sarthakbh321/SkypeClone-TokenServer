3df86ed7df1263ee6da30436b55c84ba7aed8460
Sarthak <sarthakbh321@gmail.com>
2021-07-09 02:55:51 +0530

    feat: generate agora token

--- index.ts
+++ index.ts
@@ -1,10 +1,45 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import AgoraToken from "agora-access-token";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req: any, res: any) => {
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
 	res.send("Server is up and running");
+});
+
+interface Body {
+	appId: string;
+	appCertificate: string;
+	channelName: string;
+}
+
+app.post("/generateToken", (req: Request, res: Response) => {
+	const data: Body = req.body;
+
+	const uid = Math.floor(Math.random() * 100000);
+
+	const expirationTimeInSeconds = 3600;
+	const currentTimestamp = Math.floor(Date.now() / 1000);
+	const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
+	const role = AgoraToken.RtcRole.PUBLISHER;
+
+	const token = AgoraToken.RtcTokenBuilder.buildTokenWithUid(
+		data.appId,
+		data.appCertificate,
+		data.channelName,
+		uid,
+		role,
+		privilegeExpiredTs
+	);
+
+	console.log(`Generated token for ${uid}: ${token}`);
+
+	res.status(200).json({ uid, token });
 });
 
 app.listen(PORT, () => {
